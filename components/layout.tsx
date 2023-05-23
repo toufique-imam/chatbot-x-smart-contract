eb945f6b0e6aa14d04128f4493df05a44082b82f
Toufique Imam <2013nuhash@gmail.com>
2023-05-24 00:11:15 +0600

    replace a tag with Link

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface LayoutProps {
   children?: React.ReactNode;
 }
@@ -8,13 +10,13 @@ export default function Layout({ children }: LayoutProps) {
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6">
-            <a href="/" className="hover:text-slate-600 cursor-pointer">
+            <Link href="/" className="hover:text-slate-600 cursor-pointer">
               Home
-            </a>
+            </Link>
             { globalThis.userAddress ? (
-            <a href="history" className="hover:text-slate-600 cursor-pointer px-10">
+            <Link href="history" className="hover:text-slate-600 cursor-pointer px-10">
               History
-            </a>
+            </Link>
             ) : (
               <></>
             )}

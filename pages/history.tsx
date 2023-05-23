a42d18a8346cb2c94ce1762c3c43983941858bc1
Toufique Imam <2013nuhash@gmail.com>
2023-05-23 22:17:02 +0600

    navbar in history page

--- pages/history.tsx
+++ pages/history.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import { getChatHistory } from '@/utils/ChatUtils';
 import { Message } from '@/types/chat';
+import Layout from '@/components/layout';
 
 export default function HistoryView() {
     const [error, setError] = useState<string | null>(null);
@@ -43,76 +44,88 @@ export default function HistoryView() {
 
     return (
         <>
-            {address ? (
-                <main className={styles.main}>
-                    <div className="border border-green-400 rounded-md p-4">
-                        <p className="text-green-500">{globalThis.userAddress}</p>
-                    </div>
-                    <div className="border border-blue-400 rounded-md p-4">
-                        <button onClick={handleGetHistory}> History </button>
-                    </div>
-                    <div className={styles.cloud}>
-                        <div className={styles.messagelist}>
-                            {historyMessages.map((message, index) => {
-                                let icon;
-                                let className;
-                                if (message.type === 'apiMessage') {
-                                    icon = (
-                                        <Image
-                                            key={index}
-                                            src="/bot-image.png"
-                                            alt="AI"
-                                            width="40"
-                                            height="40"
-                                            className={styles.boticon}
-                                            priority
-                                        />
-                                    );
-                                    className = styles.apimessage;
-                                } else {
-                                    icon = (
-                                        <Image
-                                            key={index}
-                                            src="/usericon.png"
-                                            alt="Me"
-                                            width="30"
-                                            height="30"
-                                            className={styles.usericon}
-                                            priority
-                                        />
-                                    );
-                                    // The latest message sent by the user will be animated while waiting for a response
-                                    className = styles.usermessage;
-                                }
-                                return (
-                                    <>
-                                        <div key={`chatMessage-${index}`} className={className}>
-                                            {icon}
-                                            <div className={styles.markdownanswer}>
-                                                <ReactMarkdown linkTarget="_blank">
-                                                    {message.message}
-                                                </ReactMarkdown>
-                                            </div>
-                                        </div>
-                                    </>
-                                );
-                            })}
-                        </div>
-                    </div>
-                </main>
-            ) : (
-                <main className={styles.main}>
-                    <div className="border border-green-400 rounded-md p-4">
-                        <button onClick={getConnection}> Connect to Polygon </button>
-                    </div>
-                    {error && (
-                        <div className="border border-red-400 rounded-md p-4">
-                            <p className="text-red-500">{error}</p>
-                        </div>
-                    )}
-                </main>
-            )
-            }
+            <Layout>
+                <div className="mx-auto flex flex-col gap-4">
+                    <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
+                        Chatbot with polygon network
+                    </h1>
+                    {address ? (
+                        <main className={styles.main}>
+                            <div className="border border-green-400 rounded-md p-4">
+                                <p className="text-green-500">{globalThis.userAddress}</p>
+                            </div>
+                            <div className="border border-blue-400 rounded-md p-4">
+                                <button onClick={handleGetHistory}> History </button>
+                            </div>
+                            <div className={styles.cloud}>
+                                <div className={styles.messagelist}>
+                                    {historyMessages.map((message, index) => {
+                                        let icon;
+                                        let className;
+                                        if (message.type === 'apiMessage') {
+                                            icon = (
+                                                <Image
+                                                    key={index}
+                                                    src="/bot-image.png"
+                                                    alt="AI"
+                                                    width="40"
+                                                    height="40"
+                                                    className={styles.boticon}
+                                                    priority
+                                                />
+                                            );
+                                            className = styles.apimessage;
+                                        } else {
+                                            icon = (
+                                                <Image
+                                                    key={index}
+                                                    src="/usericon.png"
+                                                    alt="Me"
+                                                    width="30"
+                                                    height="30"
+                                                    className={styles.usericon}
+                                                    priority
+                                                />
+                                            );
+                                            // The latest message sent by the user will be animated while waiting for a response
+                                            className = styles.usermessage;
+                                        }
+                                        return (
+                                            <>
+                                                <div key={`chatMessage-${index}`} className={className}>
+                                                    {icon}
+                                                    <div className={styles.markdownanswer}>
+                                                        <ReactMarkdown linkTarget="_blank">
+                                                            {message.message}
+                                                        </ReactMarkdown>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </main>
+                    ) : (
+                        <main className={styles.main}>
+                            <div className="border border-green-400 rounded-md p-4">
+                                <button onClick={getConnection}> Connect to Polygon </button>
+                            </div>
+                            {error && (
+                                <div className="border border-red-400 rounded-md p-4">
+                                    <p className="text-red-500">{error}</p>
+                                </div>
+                            )}
+                        </main>
+                    )
+                    }
+                </div>
+                <footer className="m-auto p-4">
+                    <a href="">
+                        Powered by LangChainAI.
+                    </a>
+                </footer>
+            </Layout>
         </>
     );
 }
